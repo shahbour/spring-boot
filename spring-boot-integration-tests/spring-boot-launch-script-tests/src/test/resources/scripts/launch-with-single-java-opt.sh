6cb27709c010a2dc174bd7801c59b749891c2687
Ali Shahbour <shahbour@gmail.com>
2017-06-01 11:17:11 +0300

    bb

--- spring-boot-integration-tests/spring-boot-launch-script-tests/src/test/resources/scripts/launch-with-single-java-opt.sh
+++ spring-boot-integration-tests/spring-boot-launch-script-tests/src/test/resources/scripts/launch-with-single-java-opt.sh
@@ -1,5 +1,6 @@
 source ./test-functions.sh
 install_service
+echo 'JAVA_OPTS=-Dserver.port=8080' > /test-service/spring-boot-app.conf
 start_service
 await_app http://127.0.0.1:8080/
 curl -s http://127.0.0.1:8080/
